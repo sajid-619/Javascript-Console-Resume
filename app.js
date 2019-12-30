3a4a7199b62483fb01069f27147f6957b69c6c2b
unknown <zsajid014@gmail.com>
2019-12-30 06:12:13 -0800

    Javascript Console Resume

---
+++ app.js
@@ -0,0 +1,37 @@
+let name = 'sajid zaman';
+console.log('Name: ' + name.toUpperCase());
+
+var mySelf = [
+
+    'Career: Full Stack Engineer',
+    'Description: Web Developer at Microsoft'
+];
+
+for (var i = 0; i < mySelf.length; i++) {
+    console.log(' ' + mySelf[i]);
+}
+
+console.log('\n' + 'My Interests: ')
+
+let interest = ['Fall TV (The best)', 'Locomotives', 'Observing awkward situations', 'Film soundtracks'];
+
+for(let i = 0; i < interest.length; i++) {
+    console.log('   * ' + interest[i]);
+}
+
+console.log('\n' + 'My Previous Experience: ')
+
+let experience = ['Mathematics, Physics and Chemistry teacher', 'Undergraduate teaching assistant at IUB', 'Software Engineer at BDAnts'];
+
+for(let i = 0; i < experience.length; i++) {
+    console.log('   * ' + experience[i]);
+}
+
+console.log('\n' + 'Skills ')
+
+let skill = ['Python', 'C++', 'JavaScript', 'Tensorflow', 'Amazon Web Service'];
+
+for(let i = 0; i < skill.length; i++) {
+    console.log('   * ' + skill[i]);
+}
+
